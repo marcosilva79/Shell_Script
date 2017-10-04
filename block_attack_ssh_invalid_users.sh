06abdaf04b839b4bafc0531cd7574914337e0073
Marco Aurélio da Silva <marcosilva79@outlook.com>
2017-10-04 19:51:12 -0300

    Update block_attack_ssh_invalid_users.sh

--- block_attack_ssh_invalid_users.sh
+++ block_attack_ssh_invalid_users.sh
@@ -2,7 +2,7 @@
 
 cat /var/log/secure | grep "Invalid user" | awk {'print $10'} | uniq > /tmp/bloquear_ip_txt
 for x in $(cat /tmp/bloquear_ip_txt | uniq); do iptables -A INPUT -s $x -p tcp --dport 22 -j DROP ; done
-for x in $(cat /tmp/bloquear_ip_txt | uniq); do echo  ALL:$x >> /etc/hosts.deny ; done
+for x in $(cat /tmp/bloquear_ip_txt | uniq); do echo  ALL: $x >> /etc/hosts.deny ; done
 echo " " > /tmp/bloquear_ip_txt
 echo " " > /var/log/secure
 echo "Reiniciando Rsyslog ..."
